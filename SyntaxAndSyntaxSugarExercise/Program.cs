8968396384b6628d4e807c6e0c64a29fb5ee51f9
ilyas Osman <ilyaso.fiiidooow@gmail.com>
2023-12-27 15:38:56 -0600

    a new One

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System;
+
+namespace SyntaxSugarExercise
 {
-    public class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = $"{answer} is less than nine";
+            }
+            else
+            {
+                response = $"{answer} is greater than or equal to nine";
+            }
+
+            // Corrected variable declaration and string interpolation
+            var newAnswer = 4;
+            var newResponse = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+
         }
     }
 }
